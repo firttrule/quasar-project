d21232e2c24b1ed968279c6515c77a6c5b4f0402
firttrule <vanezik23@mail.ru>
2023-03-08 13:12:22 +0300

    Правки в меню

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -27,47 +27,39 @@
     >
       <q-scroll-area
         style="
-          height: calc(100% - 150px);
-          margin-top: 150px;
+          height: calc(100% - 185px);
+          margin-top: 185px;
           border-right: 1px solid #ddd;
         "
       >
         <q-list padding>
           <q-item clickable v-ripple>
             <q-item-section avatar>
-              <q-icon name="inbox" />
+              <q-icon name="list" />
             </q-item-section>
 
-            <q-item-section> Inbox </q-item-section>
-          </q-item>
-
-          <q-item active clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="star" />
-            </q-item-section>
-
-            <q-item-section> Star </q-item-section>
+            <q-item-section> Todo </q-item-section>
           </q-item>
 
           <q-item clickable v-ripple>
             <q-item-section avatar>
-              <q-icon name="send" />
+              <q-icon name="help" />
             </q-item-section>
 
-            <q-item-section> Send </q-item-section>
+            <q-item-section> Help </q-item-section>
           </q-item>
 
           <q-item clickable v-ripple>
             <q-item-section avatar>
-              <q-icon name="drafts" />
+              <q-icon name="link" />
             </q-item-section>
 
-            <q-item-section> Drafts </q-item-section>
+            <q-item-section> About </q-item-section>
           </q-item>
         </q-list>
       </q-scroll-area>
 
-      <q-img class="absolute-top image" style="height: 150px">
+      <q-img class="absolute-top image" style="height: 185px">
         <div class="absolute-bottom bg-transparent">
           <q-avatar size="56px" class="q-mb-sm">
             <img src="../assets/avatar.jpg" />
