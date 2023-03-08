659f8e771801506855188a304367de44a9c1c92c
firttrule <vanezik23@mail.ru>
2023-03-08 21:08:03 +0300

    Список задач

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,29 +1,37 @@
 <template>
-  <q-page class="q-pa-lg">
-    <h5>Todo</h5>
-
-    <div class="q-pa-md" style="max-width: 350px">
-      <q-list bordered separator>
-        <q-item clickable v-ripple>
-          <q-item-section>first todo</q-item-section>
-        </q-item>
-
-        <q-item clickable v-ripple>
-          <q-item-section>
-            <q-item-label>second todo</q-item-label>
-            <q-item-label caption>done</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable v-ripple>
-          <q-item-section>
-            <q-item-label>third todo</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </div>
+  <q-page class="bg-grey-3 column">
+    <q-list>
+      <q-item v-for="task in tasks" :key="task.title" tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="color" val="teal" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: "hello",
+          done: false,
+        },
+        {
+          title: "hello2",
+          done: false,
+        },
+        {
+          title: "hello3",
+          done: false,
+        },
+      ],
+    };
+  },
+};
 </script>
